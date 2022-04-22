b9ea54175bbf985d7f8f2e38e4f126a01c51c4fe
claudioDantas <claudiodantas1996@gmail.com>
2022-04-22 11:21:50 -0300

    get number of cart items

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,24 @@
 import { BsCart2 } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import classes from "./nav.module.css";
 
 export default function Nav() {
+  const cart = useSelector((state) => state.cart);
+  const len = cart.items.length;
+
   return (
     <nav className={classes.navigation}>
       <ul className={classes.container}>
-        <li>Redux Shopping Cart</li>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <li>Redux Shopping Cart</li>
+        </Link>
         <li className={classes.badge}>
           <i>
             <BsCart2 size={25} />
           </i>
-          <p>( 1 )</p>
+          <p>( {len} )</p>
         </li>
       </ul>
     </nav>
