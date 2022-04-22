6ec4faec0b0373537e9f38faac1e1ddfb470787d
claudioDantas <claudiodantas1996@gmail.com>
2022-04-22 11:22:34 -0300

    remove code to another component

--- src/components/products/productDetail.jsx
+++ src/components/products/productDetail.jsx
@@ -3,12 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { clear, fetchSingleProduct } from "../../redux/actions/products";
 
+import Nav from "../nav/Nav";
+import classes from "./productDetail.module.css";
+import ProductInfo from "./productInfo";
+
 export default function ProductDetail() {
   const product = useSelector((state) => state.products.product);
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   const { id } = useParams();
 
-  console.log(product);
+  console.log(cart);
 
   useEffect(() => {
     dispatch(fetchSingleProduct(id));
@@ -18,5 +23,17 @@ export default function ProductDetail() {
     };
   }, []);
 
-  return <p>{product.title}</p>;
+  return (
+    <>
+      <Nav />
+      <div className={classes.container}>
+        <div className={classes.product}>
+          <div className={classes["product__image"]}>
+            <img src={product.image} alt={product.title} />
+          </div>
+          <ProductInfo />
+        </div>
+      </div>
+    </>
+  );
 }
