034df2cadc2e6bd78b10e88b49ee396cc8c84202
claudioDantas <claudiodantas1996@gmail.com>
2022-04-22 11:24:29 -0300

    add cart reducer to combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 
 import notification from "./notification";
 import products from "./products";
+import cart from "./cart";
 
-const reducers = combineReducers({ products, notification });
+const reducers = combineReducers({ products, notification, cart });
 
 export default reducers;
